ed5e8a42e22fe2867e1af4b1f54a1b56409dcf1d
Sumit Alam Khan <31077557+Tsumiiiiiiii@users.noreply.github.com>
2024-12-03 23:01:03 +0600

    Create part2.rs

---
+++ 2024/Day 3/part2.rs
@@ -0,0 +1,47 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+fn calc(cmd : &str) -> i64 {
+    
+    let mut comma : usize = usize::MAX;
+    for i in 4..cmd.len() - 1 {
+        let c = cmd.chars().nth(i).unwrap();
+        if c == ',' && comma != usize::MAX {return 0;}
+        else if c == ',' {comma = i;}
+        else if !c.is_ascii_digit() {return 0;}
+    }
+    if comma == usize::MAX {return 0;}
+    //println!("{:?}", cmd);
+    let num1 = &cmd[4..comma as usize].to_string().parse::<i64>().unwrap();
+    let num2 = &cmd[comma+1..cmd.len() - 1 as usize].to_string().parse::<i64>().unwrap();
+    return num1 * num2;
+
+}
+
+fn main() -> io::Result<()> {
+    let file = File::open("data.txt")?;
+    let reader = BufReader::new(file);
+
+    let mut tot : i64 = 0;
+    let mut on = 1;
+    for line in reader.lines() {
+        let cmd = line?;
+        for i in 0..cmd.len() - 5 {
+            for j in i+1..cmd.len() { //because a mul instruction wont actually take more than that ever ??
+                if j >= cmd.len() {continue;}
+                if &cmd[i..j+1] == "don't()" {
+                    on = 0;
+                    continue;
+                } else if &cmd[i..j+1] == "do()" {
+                    on = 1;
+                    continue;
+                } else if !(&cmd[i..i+4] == "mul(" && cmd.chars().nth(j).unwrap() == ')') {continue;}
+                tot += on * calc(&cmd[i..j+1]);
+            }
+        }
+    }
+
+    println!("{tot}");
+
+    Ok(())
+}
